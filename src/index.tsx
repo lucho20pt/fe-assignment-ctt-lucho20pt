ddfc40e2599f2a96fa8e71a6103b150ed6a158f4
Daniel <dani_batista20@hotmail.com>
2025-06-03 03:35:49 +0100

    refactor: restructure App component layout for improved readability and styling

--- src/index.tsx
+++ src/index.tsx
@@ -7,15 +7,25 @@ import store from './store'
 const App = () => (
   <React.StrictMode>
     <Provider store={store}>
-      <header>
-        <h1>CTT Exercise - Frontend Typescript</h1>
-      </header>
-      <main>
-        <Product />
-      </main>
-      <footer>
-        <p>© {new Date().getFullYear()} CTT Exercise</p>
-      </footer>
+      <div className="container mx-auto flex flex-col gap-10">
+        <header
+          className="flex flex-col items-center justify-center p-8 
+        bg-indigo-700 text-white text-lg rounded-lg shadow-md"
+        >
+          <h1 className="text-5xl font-bold text-center">
+            CTT Exercise - Frontend Typescript
+          </h1>
+        </header>
+        <main
+          className="flex flex-col items-center justify-center p-8
+        bg-indigo-600 text-white text-lg rounded-lg shadow-md"
+        >
+          <Product />
+        </main>
+        <footer>
+          <p>© {new Date().getFullYear()} CTT Exercise</p>
+        </footer>
+      </div>
     </Provider>
   </React.StrictMode>
 )
